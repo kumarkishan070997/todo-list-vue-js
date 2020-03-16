57e1e9ce41ae5b5c078f46668460ab7f769fd36e
Krishan Kumar Maharana <kishan.maharana@mail.vinove.com>
2020-03-16 10:35:25 +0530

    added counter and delete all button

--- vue.js
+++ vue.js
@@ -5,16 +5,26 @@ new Vue({
         data: '',
         final: '',
         all: [],
+        count: 0,
     },
     methods: {
         addTodo() {
+            this.count = this.count + 1;
             this.all.push({
                 names: this.data
             });
+            this.data = '';
         },
         deleteData(out) {
+
             console.log(this.all.indexOf(out));
             this.all.splice(this.all.indexOf(out), 1);
+        },
+        hello(counter) {
+            return this.all.indexOf(counter) + 1;
+        },
+        deleteAll() {
+            this.all = [];
         }
     }
 });
